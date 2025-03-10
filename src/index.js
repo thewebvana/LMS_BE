3265fcb30987343ac8ba3181a53cfc5a9ebc0edd
tanayoptbyte <tanayc@optbyte.com>
2025-03-10 20:17:47 +0530

    user route add

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
 
-dotenv.config();
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-// Import Routes
-const authRoutes = require("./routes/authRoutes");
+app.use("/lms/auth", authRoutes);
+app.use("/lms/users", userRoutes);
 
-// Use "/lms" prefix for all routes
-app.use("/lms", authRoutes);
+app.get("/", (req, res) => {
+  res.send("School Management API is running...");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
