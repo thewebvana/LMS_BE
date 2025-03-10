142a5ab40939ce6f40e0fd06b75c8710dc9a9be5
Sourabh garewal <28saurbh@gmail.com>
2025-03-10 22:45:05 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -5,11 +5,21 @@ const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
-app.use(cors());
+
+// Enable CORS for all routes
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"], // Allow Authorization header
+    credentials: true, // Allows sending cookies (if needed)
+  })
+);
+
 app.use(express.json());
 
-app.use("/lms/auth", authRoutes);
-app.use("/lms/users", userRoutes);
+app.use("/lms/api/auth/", authRoutes);
+app.use("/lms/api/users/", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("LMS API is running...");
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
-router.post("/register", authController.register);
-router.post("/login", authController.login);
+router.post("/v1/register", authController.register);
+router.post("/v1/login", authController.login);
 
 module.exports = router;
