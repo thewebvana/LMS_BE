6af5b19599044bcc508f54482c8f37b0541a2f9a
tanayoptbyte <tanayc@optbyte.com>
2025-03-10 20:30:04 +0530

    routing fix

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
-router.post("/lms/auth/register", authController.register);
-router.post("/lms/auth/login", authController.login);
+router.post("/register", authController.register);
+router.post("/login", authController.login);
 
 module.exports = router;
