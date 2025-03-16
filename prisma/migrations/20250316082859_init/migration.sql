47b535430ea74479ce63a0d5ee387f731761de1b
Sourabh garewal <28saurbh@gmail.com>
2025-03-16 13:59:37 +0530

    migartion files

--- prisma/migrations/20250316082859_init/migration.sql
+++ prisma/migrations/20250316082859_init/migration.sql
@@ -1,11 +1,5 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Principle` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "Principle";
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('male', 'female', 'other');
 
 -- CreateTable
 CREATE TABLE "principle" (
